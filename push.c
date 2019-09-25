155ef2810f9507b2299ff1a57d14648c20700395
geraldinnebohr <geraldinnebohr@gmail.com>
2019-09-25 17:42:59 +0000

    [FIX] error if opcode is null

--- push.c
+++ push.c
@@ -16,11 +16,18 @@ void push(stack_t **stack, unsigned int line_number)
 	char* opcode = "h";
 
 	(void)line_number;
+
+	if (opcode == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
 	new = malloc(sizeof(stack_t));
 
 	if (new == NULL)
 	{
-		dprintf(STDERR_FILENO, "L%d: usage: push integer\n", line_number);
+		dprintf(STDERR_FILENO, "Error\n");
 		exit(EXIT_FAILURE);
 	}
 
