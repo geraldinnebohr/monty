0b819081d40e94c1ee6b57cdb5fdb342fd2b7bf4
geraldinnebohr <geraldinnebohr@gmail.com>
2019-09-25 04:14:24 +0000

    [FEATURE] function that removes the top element of the stack

---
+++ pop.c
@@ -0,0 +1,23 @@
+#define "monty.h"
+
+/**
+ * pop - function that removes the top element of the stack
+ * @stack: header of the stack
+ * @line_number: number of line in .m file
+ */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ant, *temp;
+
+	ant = *stack;
+	if (ant == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error");
+		exit();
+	}
+
+	temp = ant;
+	ant->next = temp->next;
+	free(ant);
+}
