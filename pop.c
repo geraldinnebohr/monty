b50ed16727b7aae38d36ec5de870cd1d7534a775
geraldinnebohr <geraldinnebohr@gmail.com>
2019-09-26 01:17:31 +0000

    [FIX] added a new line in error messages

--- pop.c
+++ pop.c
@@ -11,7 +11,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL || stack == NULL)
 	{
 		dprintf(STDERR_FILENO,
-			"L%d: can't pop an empty stack", line_number);
+			"L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	temp = *stack;
--- swap.c
+++ swap.c
@@ -13,7 +13,7 @@ void swap(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		dprintf(STDERR_FILENO,
-			"L%d: can't swap, stack too short", line_number);
+			"L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
