2eefec49b0c2a72572c6fce38710e6511314a31d
geraldinnebohr <geraldinnebohr@gmail.com>
2019-09-26 04:07:45 +0000

    [FIX] mod option in structure

--- main.c
+++ main.c
@@ -39,7 +39,7 @@ void select_instruction(FILE *bt_code)
 				    {"swap", swap}, {"add", add},
 				    {"nop", nop}, {"sub", sub},
 				    {"div", division}, {"mul", mul},
-				    {"mul", mul}, {NULL, NULL}};
+				    {"mod", mul}, {NULL, NULL}};
 	stack = NULL;
 	line_number = 1;
 	while (fgets(line, sizeof(line), bt_code) != NULL)
