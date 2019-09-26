135ee999f3d6ad8412825bdfdca111916c67c452
geraldinnebohr <geraldinnebohr@gmail.com>
2019-09-26 01:14:27 +0000

    [FIX] added a new line

--- pint.c
+++ pint.c
@@ -11,7 +11,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		dprintf(STDERR_FILENO,
-			"L%d: can't pint, stack empty", line_number);
+			"L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
