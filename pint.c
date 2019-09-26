fb9391600f147aa92ba95821a73b55309d981d09
julgachancipa <julianaga01@gmail.com>
2019-09-26 23:13:48 +0000

    [FIX] line number

--- pint.c
+++ pint.c
@@ -8,10 +8,16 @@
 
 void pint(stack_t **stack, unsigned int line_number)
 {
+	(void)line_number;
+
 	if (*stack == NULL)
 	{
 		dprintf(STDERR_FILENO,
-			"L%d: can't pint, stack empty\n", line_number);
+			"L%d: can't pint, stack empty\n", gbl.line_number);
+		free_dlistint(*stack);
+		free(gbl.line);
+		free(gbl.div_line);
+		fclose(gbl.bt_code);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
